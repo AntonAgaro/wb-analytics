df89717b9b4546f7ab131f3302217dfe6466158d
Anton Agaronov <anton.agaronov@legalbet.com>
2024-11-09 15:13:59 +0400

    feat: install nuxt ui

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">
+      <h1>index</h1>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+//  'https://search.wb.ru/exactmatch/ru/common/v7/search?appType=64&curr=rub&dest=-1257786&query=%D0%BA%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8+%D0%BC%D1%83%D0%B6%D1%81%D0%BA%D0%B8%D0%B5+%D0%B7%D0%B8%D0%BC%D0%BD%D0%B8%D0%B5&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false'
+
+</script>
+
+<style scoped>
+</style>
